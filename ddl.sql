6a2f8a184648d765d86be2bd7632ae87b827c8f6
袁柱 <1522719885@qq.com>
2020-07-02 17:41:06 +0800

    Update ddl.sql

--- ddl.sql
+++ ddl.sql
@@ -23,17 +23,25 @@ drop database db_test;
 
 -- **Table level**
 -- Revise table name
+alter table student rename stu;
 
 -- Revise the field's data type
+alter table stu modify id int;
 
 -- Revise field name
+alter table stu change name stu_name varchar(15);
 
 -- Add field
+alter table stu add score int;
 
 -- Delete field
+alter table stu drop score;
 
 -- Revise the table's storage engine
+alter table stu engine=MyISAM;
 
 -- Delete the table's foreign key restriant
+alter table stu drop foreign key fk_name;
 
 -- Delete a table
+drop table stu;
