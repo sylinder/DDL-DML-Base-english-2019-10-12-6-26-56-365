2c0f0ba7cc5eb7a7daf3f0204d152a102fcc1090
袁柱 <1522719885@qq.com>
2020-07-02 17:49:48 +0800

    Update dml.sql

--- dml.sql
+++ dml.sql
@@ -1,7 +1,11 @@
 -- Insert record
+insert into stu values("003", "Jack", 28, "male");
 
 -- Revise record
+update stu set sex = "female" where name = "Colin";
 
 --  Delete record
+delete from stu where id = "003";
 
 -- Search record
+select * from stu where id = "001";
