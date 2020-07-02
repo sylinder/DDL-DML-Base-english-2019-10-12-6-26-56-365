2b2376457d94140b417b8eb766b7443ab287d19f
袁柱 <1522719885@qq.com>
2020-07-02 17:29:15 +0800

    Update ddl.sql

--- ddl.sql
+++ ddl.sql
@@ -1,17 +1,25 @@
 -- **Database level:**
 -- Display all the database
+show databases;
 
 -- Enter a certain database
+use test;
 
 -- Create a database
+create database db_test;
+
 
 -- Create the database of the designated character set
+create database db_test character set utf8;
 
 -- Display the creation information fo the database
+show create database db_test;
 
 -- Revise the codes of the database
+alter database db_test character set gb2312;
 
 -- Delete a database
+drop database db_test;
 
 -- **Table level**
 -- Revise table name
